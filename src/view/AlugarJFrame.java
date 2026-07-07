032e6e27b625418d7bf6a177ed1474b815d7d575
josvi <josvi@192.168.1.146>
2026-07-07 20:06:18 -0300

    novas atribuições para o sistema

--- src/view/AlugarJFrame.java
+++ src/view/AlugarJFrame.java
@@ -341,3 +341,350 @@ public class AlugarJFrame extends javax.swing.JFrame {
     private javax.swing.JTextField txtTotalAluguel;
     // End of variables declaration//GEN-END:variables
 }
+
+package view;
+
+import view.ClienteJFrame;
+import java.sql.*;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+import dao.AluguelDAO;
+import model.Aluguel;
+
+public class AlugarJFrame extends javax.swing.JFrame {
+    
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(ClienteJFrame.class.getName());
+
+    
+    public AlugarJFrame() {
+        initComponents();
+        AlugarData();
+    }
+
+    private void AlugarData(){
+        try {
+
+            AluguelDAO dao = new AluguelDAO();
+
+            DefaultTableModel modelo = (DefaultTableModel) table1.getModel();
+
+            modelo.setRowCount(0);
+
+            for (Aluguel aluguel : dao.listarTodos()) {
+
+                modelo.addRow(new Object[]{
+                    aluguel.getIdAluguel(),
+                    aluguel.getNomeCliente(),
+                    aluguel.getIdComputador(),
+                    aluguel.getQtdHoras()
+                });
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AlugarJFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table1 = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtIdAluguel = new javax.swing.JTextField();
+        txtNomeCliente = new javax.swing.JTextField();
+        txtIdComputador = new javax.swing.JTextField();
+        txtQtdHoras = new javax.swing.JTextField();
+        btnInsert = new javax.swing.JButton();
+        btnUpdate = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        btnTotalAluguel = new javax.swing.JButton();
+        txtTotalAluguel = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(153, 255, 153));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("ALUGUEL");
+
+        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jButton1.setText("X");
+        jButton1.addActionListener(this::jButton1ActionPerformed);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(217, 217, 217)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton1)
+                    .addComponent(jLabel1))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        table1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ID do Aluguel", "Cliente", "ID do Computador", "Qtd. Horas"
+            }
+        ));
+        jScrollPane1.setViewportView(table1);
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setText("ID do Aluguel");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel3.setText("Nome do Cliente");
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel4.setText("ID do Computador");
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel5.setText("Quantidade de Horas");
+
+        txtIdAluguel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+
+        txtNomeCliente.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+
+        txtIdComputador.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+
+        txtQtdHoras.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+
+        btnInsert.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnInsert.setText("Insert");
+        btnInsert.addActionListener(this::btnInsertActionPerformed);
+
+        btnUpdate.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnUpdate.setText("Update");
+        btnUpdate.addActionListener(this::btnUpdateActionPerformed);
+
+        btnDelete.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnDelete.setText("Delete");
+        btnDelete.addActionListener(this::btnDeleteActionPerformed);
+
+        btnTotalAluguel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnTotalAluguel.setText("Total");
+        btnTotalAluguel.addActionListener(this::btnTotalAluguelActionPerformed);
+
+        txtTotalAluguel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jScrollPane1)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(txtIdAluguel)
+                            .addComponent(txtNomeCliente)
+                            .addComponent(txtIdComputador)
+                            .addComponent(txtQtdHoras, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(34, 34, 34))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(132, 132, 132)
+                        .addComponent(btnInsert)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnUpdate)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnDelete)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 59, Short.MAX_VALUE)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtTotalAluguel, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnTotalAluguel))
+                .addGap(17, 17, 17))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtIdAluguel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtNomeCliente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(15, 15, 15)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtIdComputador, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(15, 15, 15)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(txtQtdHoras, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnTotalAluguel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnInsert)
+                    .addComponent(btnUpdate)
+                    .addComponent(btnDelete)
+                    .addComponent(txtTotalAluguel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>                        
+
+    private void btnInsertActionPerformed(java.awt.event.ActionEvent evt) {                                          
+        try {
+
+            Aluguel aluguel = new Aluguel();
+
+            aluguel.setIdAluguel(Integer.parseInt(txtIdAluguel.getText()));
+            aluguel.setNomeCliente(txtNomeCliente.getText());
+            aluguel.setIdComputador(Integer.parseInt(txtIdComputador.getText()));
+            aluguel.setQtdHoras(txtQtdHoras.getText());
+
+            AluguelDAO dao = new AluguelDAO();
+
+            dao.inserir(aluguel);
+
+            JOptionPane.showMessageDialog(this, "Aluguel salvo com sucesso!");
+
+            AlugarData();
+
+        } catch (SQLException ex) {
+
+             JOptionPane.showMessageDialog(this, ex.getMessage());
+
+           }
+
+    }                                         
+
+    private void btnUpdateActionPerformed(java.awt.event.ActionEvent evt) {                                          
+        try {
+
+            Aluguel aluguel = new Aluguel();
+
+            aluguel.setIdAluguel(Integer.parseInt(txtIdAluguel.getText()));
+            aluguel.setNomeCliente(txtNomeCliente.getText());
+            aluguel.setIdComputador(Integer.parseInt(txtIdComputador.getText()));
+            aluguel.setQtdHoras(txtQtdHoras.getText());
+
+            AluguelDAO dao = new AluguelDAO();
+
+            dao.atualizar(aluguel);
+
+            JOptionPane.showMessageDialog(this, "Aluguel atualizado com sucesso!");
+
+            AlugarData();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AlugarJFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }                                         
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {                                          
+        try {
+
+            AluguelDAO dao = new AluguelDAO();
+
+            dao.excluir(Integer.parseInt(txtIdAluguel.getText()));
+
+            JOptionPane.showMessageDialog(this, "Aluguel excluído com sucesso!");
+
+            AlugarData();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AlugarJFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }                                         
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        dispose();
+    }                                        
+
+    private void btnTotalAluguelActionPerformed(java.awt.event.ActionEvent evt) {                                                
+        try {
+
+            AluguelDAO dao = new AluguelDAO();
+
+            txtTotalAluguel.setText(String.valueOf(dao.contar()));
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AlugarJFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }                                               
+
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new AlugarJFrame().setVisible(true));
+    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnInsert;
+    private javax.swing.JButton btnTotalAluguel;
+    private javax.swing.JButton btnUpdate;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable table1;
+    private javax.swing.JTextField txtIdAluguel;
+    private javax.swing.JTextField txtIdComputador;
+    private javax.swing.JTextField txtNomeCliente;
+    private javax.swing.JTextField txtQtdHoras;
+    private javax.swing.JTextField txtTotalAluguel;
+    // End of variables declaration                   
+}
